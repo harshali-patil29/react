cc6893cdd2d84d48c94bf175b1b1eb64e74490b8
harshali-patil29 <harshalip167@gmail.com>
2024-03-05 20:53:44 +0530

    Add search feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,11 +5,13 @@ import { useState,useEffect } from "react";
 //import { PulseLoader } from "react-spinners";
 import Shimmer from "./Shimmer";
 
-
-// console.log(resList[0].info.name);
 const Body = () => {
 
     const [listOfRestaurant,setListOfRestaurant] = useState([]);
+
+    const [filteredRestaurant,setfilteredRestaurant] = useState([]);
+
+    const [searchText, setsearchText] = useState("");
  
     useEffect(
         ()=>{
@@ -18,34 +20,45 @@ const Body = () => {
         );
 
     const fetchData = async () =>{
-        //const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.05650&lng=73.06560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        // const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=19.05650&lng=73.06560");
-        
-        //const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=19.0759837&lng=72.8776559");
-        //const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=19.0759837&lng=72.8776559");
-        //error 400 changes should be done here...cause api is updated
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        
+        //error 400 implies api is updated so changes should be done here in api url
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");        
         const json = await data.json();
         //console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
         setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setfilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
-
-
     // if(listOfRestaurant.length === 0) {
     //     return <Shimmer />
     // }
     //conditional rendeing and ternary operator
     return listOfRestaurant.length === 0 ? <Shimmer /> :(
         <div className="body">
-            <div className="search">What's on your mind?</div>
+            <div className="search">
+                <input type="text" placeholder="What's on your mind?" className="search-box"
+                value={searchText}
+                onChange={
+                    (e)=>{
+                        setsearchText(e.target.value)
+                    }
+                }
+                />
+                <button className="search-btn"
+                onClick={()=>{
+                    const filteredRestaurant = listOfRestaurant.filter(
+                        (res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                    );
+                    setfilteredRestaurant(filteredRestaurant);
+                }}
+
+                >Search</button>
+                
+            </div>
 
             <div className="filter">
                     <button className="filter-btn" id="all"
                     onClick={ 
                         () => {
-                        // const filteredList = resList;
-                        fetchData();
+                        setfilteredRestaurant(listOfRestaurant);
                         }
                     } >All</button>
                     <button className="filter-btn" id="based-on-rating"
@@ -54,7 +67,7 @@ const Body = () => {
                         const filteredList = listOfRestaurant.filter((res)=>res.info.avgRating > 4);
                             console.log(filteredList);
                         //update the list using set 
-                        setListOfRestaurant(filteredList);
+                        setfilteredRestaurant(filteredList);
                     }
                     } >Top Rated Restaurant</button>
                     <button className="filter-btn" id="based-on-delivery"
@@ -63,7 +76,7 @@ const Body = () => {
                         const filteredList = listOfRestaurant.filter((res)=>res.info.sla.deliveryTime < 30);
                             console.log(filteredList);
                         //update the list using set 
-                        setListOfRestaurant(filteredList);
+                        setfilteredRestaurant(filteredList);
                     }
                     } >Fast Delivery</button>
                     <button className="filter-btn" id="based-on-veg"
@@ -72,14 +85,14 @@ const Body = () => {
                         const filteredList = listOfRestaurant.filter((res)=>res.info.veg === true);
                             console.log(filteredList);
                         //update the list using set 
-                        setListOfRestaurant(filteredList);
+                        setfilteredRestaurant(filteredList);
                     }
                     } >Pure Veg</button>
             </div>
 
             
             <div className="res-container">
-                    {listOfRestaurant.map((restaurant,index) =>(
+                    {filteredRestaurant.map((restaurant,index) =>(
                         <RestaurantCard key={index} resData = {restaurant}/>
                         ))
                     }
